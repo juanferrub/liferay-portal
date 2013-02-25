0991f825d1efc2eeafd22754f58733073549b6d7
ealonso <eudaldo.alonso@liferay.com>
2013-02-25 12:20:56 +0100

    LPS-31965 Support for search within a structure field in DLFileEntry

--- portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalArticleIndexer.java
@@ -129,6 +129,17 @@ public class JournalArticleIndexer extends BaseIndexer {
 			contextQuery.addRequiredTerm("ddmTemplateKey", ddmTemplateKey);
 		}
 
+		String structureField = (String)searchContext.getAttribute(
+			"structureField");
+		String structureValue = (String)searchContext.getAttribute(
+			"structureValue");
+
+		if (Validator.isNotNull(structureField) &&
+				Validator.isNotNull(structureValue)) {
+
+			contextQuery.addRequiredTerm(structureField, structureValue);
+		}
+
 		long[] folderIds = searchContext.getFolderIds();
 
 		if ((folderIds != null) && (folderIds.length > 0)) {
